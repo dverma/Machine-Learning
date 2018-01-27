60e4dd622b161c79258cc9adb5f8be646048e40a
Dhawal Verma <dhawalverma@gmail.com>
2018-01-27 00:05:23 -0600

    pre processing template

--- Data_Preprocessing/data_preprocessing.py
+++ Data_Preprocessing/data_preprocessing.py
@@ -27,7 +27,26 @@ imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
 
 #Encoding categorical data
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 labelencoder_x = LabelEncoder()
-labelencoder_x.fit_transform(x[:, 0])
+x[:,0] = labelencoder_x.fit_transform(x[:, 0])
+
+onehotencoder = OneHotEncoder(categorical_features=[0])
+x= onehotencoder.fit_transform(x).toarray()
+
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+
+#Splitting the dataset into training and test set
+
+from sklearn.cross_validation import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y,test_size=0.2,random_state=42)
+
+#Feature Scaling
+
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
